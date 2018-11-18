a065342d18e9e2b240e4e91ecdda48b8dc00116d
Nastya <leshencko.nastia2013@gmail.com>
2018-11-18 16:22:55 +0300

    started doing procedure for booking

--- KP/scripts/procedures.sql
+++ KP/scripts/procedures.sql
@@ -57,5 +57,58 @@ begin
     cinema_admin.add_genre('комедия');
     cinema_admin.add_genre('триллер');
     cinema_admin.add_genre('фантастика');
-    cinema_admin.add_genre('драма');
 end;
+
+create procedure cinema_admin.add_movie_genre (movie_id_a number, genre_id_a number) as
+    begin
+        insert into cinema_admin.movie_genre (movie_id, genre_id)
+        values (movie_id_a, genre_id_a);
+    end;
+/
+
+begin
+    cinema_admin.add_movie_genre (1, 1);
+    cinema_admin.add_movie_genre (2, 2);
+    cinema_admin.add_movie_genre (3, 3);
+    cinema_admin.add_movie_genre (4, 4);
+    cinema_admin.add_movie_genre (5, 4);
+end;
+
+create procedure cinema_admin.add_customer (login_a nvarchar2, password_a nvarchar2, email_a varchar2) as
+    begin
+        insert into cinema_admin.customer (login, password, email)
+        values (login_a, password_a, email_a);
+    end;
+/
+
+begin 
+    cinema_admin.add_customer ('nastia2211', '123', 'nastia@gmail.com');
+    cinema_admin.add_customer ('oleg1972', '987', 'oleg@gmail.com');
+end;
+
+create procedure cinema_admin.add_seance (cinema_hall_id_a number, movie_id_a number, timetable_a date) as
+    begin
+        insert into cinema_admin.seance (cinema_hall_id, movie_id, timetable)
+        values (cinema_hall_id_a, movie_id_a, timetable_a);
+    end;
+/
+
+begin
+    cinema_admin.add_seance (1, 1, '28-10-1999');
+    cinema_admin.add_seance (1, 1, '30-05-2003');
+end;
+
+create procedure cinema_admin.book_place 
+(customer_id_a number, booking_date_a date, place_row number, place_place number, cost_a float, seance_id number) is
+    p_id number;
+    b_id number;
+    begin
+        insert into cinema_admin.booking (customer_id, booking_date)
+        values (customer_id_a, booking_date_a);
+        insert into cinema_admin.place (roww, place, cost, seance_id)
+        values (place_row, place_place, cost_a, seance_id);
+        
+        insert into cinema_admin.booked_places (place_id, booking_id)
+        values ((select id from cinema_admin.place where
+    end;
+
