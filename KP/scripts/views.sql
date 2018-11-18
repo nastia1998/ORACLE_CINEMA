0d41d1ab64482efb2cbdaa0b3fcc8566a0be6ea2
Nastya <leshencko.nastia2013@gmail.com>
2018-11-18 16:23:23 +0300

    updated views

--- KP/scripts/views.sql
+++ KP/scripts/views.sql
@@ -1,3 +1,12 @@
 -- grant create view to cinema_admin
 create view cinemas_view as
     select * from cinema_admin.cinema;
+
+select * from cinema_admin.cinemas_view;
+
+create view cinemas_and_halls_view as
+    select cinema.name, cinema.address, cinema_hall.rows_count, cinema_hall.places_count
+    from cinema join cinema_hall
+    on cinema.id = cinema_hall.cinema_id;
+    
+select * from cinemas_and_halls_view;
